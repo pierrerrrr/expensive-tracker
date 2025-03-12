577cf64b66151702aa333cc6c76fdb1b2f1b8ea6
Iago Pierre <pierrestorniolo@gmail.com>
2025-03-11 22:33:27 -0300

    fix: mt-9 gonna fit better

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ function layout({ children }: { children: ReactNode }) {
   return (
     <div className="relative flex h-screen w-full flex-col items-center justify-center">
       <Logo />
-      <div className="mt-12">{children}</div>
+      <div className="mt-9">{children}</div>
     </div>
   );
 }
