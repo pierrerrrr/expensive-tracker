7a8904340df27cee1dba20469c93763872e6bce6
Iago Pierre <pierrestorniolo@gmail.com>
2025-03-19 09:38:36 -0300

    feat: add next-themes and created navbar, logonavbar and the navigation - something went wrong in the middle process but dafuq idk

---
+++ components/providers/RootProvider.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+import React, { ReactNode } from "react";
+
+function RootProvider({ children }: { children: ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
+
+export default RootProvider;
